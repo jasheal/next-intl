a86141de442e1395e8a449c11d7e56c4b7d45684
jasheal <7109973+jasheal@users.noreply.github.com>
2023-02-18 23:03:25 +0000

    chore: demo image broken image paths

--- packages/example-next-13-advanced/src/app/[locale]/page.tsx
+++ packages/example-next-13-advanced/src/app/[locale]/page.tsx
@@ -1,27 +1,29 @@
-import {Link, useTranslations} from 'next-intl';
-import ClientRouterWithoutProvider from '../../components/ClientRouterWithoutProvider';
-import CoreLibrary from '../../components/CoreLibrary';
-import CurrentTime from '../../components/CurrentTime';
-import LocaleSwitcher from '../../components/LocaleSwitcher';
-import PageLayout from '../../components/PageLayout';
-import MessagesAsPropsCounter from '../../components/client/01-MessagesAsPropsCounter';
-import MessagesOnClientCounter from '../../components/client/02-MessagesOnClientCounter';
+import { Link, useTranslations } from 'next-intl'
+import Image from 'next/image'
+import ClientRouterWithoutProvider from '../../components/ClientRouterWithoutProvider'
+import CoreLibrary from '../../components/CoreLibrary'
+import CurrentTime from '../../components/CurrentTime'
+import LocaleSwitcher from '../../components/LocaleSwitcher'
+import PageLayout from '../../components/PageLayout'
+import MessagesAsPropsCounter from '../../components/client/01-MessagesAsPropsCounter'
+import MessagesOnClientCounter from '../../components/client/02-MessagesOnClientCounter'
 
 type Props = {
-  searchParams: Record<string, string>;
-};
+  searchParams: Record<string, string>
+}
 
-export default function Index({searchParams}: Props) {
-  const t = useTranslations('Index');
+export default function Index({ searchParams }: Props) {
+  const t = useTranslations('Index')
 
   return (
     <PageLayout title={t('title')}>
       <p>{t('description')}</p>
       <p data-testid="RichText">
-        {t.rich('rich', {important: (chunks) => <b>{chunks}</b>})}
+        {t.rich('rich', { important: (chunks) => <b>{chunks}</b> })}
       </p>
+      <Image src="/kitten.jpeg" width={100} height={80} alt="no kittens :(" />
       <p
-        dangerouslySetInnerHTML={{__html: t.raw('rich')}}
+        dangerouslySetInnerHTML={{ __html: t.raw('rich') }}
         data-testid="RawText"
       />
       <p data-testid="GlobalDefaults">{t.rich('globalDefaults')}</p>
@@ -35,11 +37,11 @@ export default function Index({searchParams}: Props) {
       <CoreLibrary />
       <ClientRouterWithoutProvider />
       <div>
-        <Link href={{pathname: '/', query: {test: true}}}>
+        <Link href={{ pathname: '/', query: { test: true } }}>
           Go to home with query param
         </Link>
       </div>
       <p data-testid="SearchParams">{JSON.stringify(searchParams, null, 2)}</p>
     </PageLayout>
-  );
+  )
 }
